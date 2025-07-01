c38056697803af17dfacea4d38ab04bc851ae71c
Toure <142293316+soultntoure@users.noreply.github.com>
2025-07-01 09:24:39 +0800

    feat: scaffold client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // Example for basic styling
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Fitness Booking</h1>
+        <p>Discover, book, and sweat with ease!</p>
+      </header>
+      {/* Routing and page components will be added here */}
+    </div>
+  );
+}
+
+export default App;
